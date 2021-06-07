993250978adb3beddc39e804f4adbe5f6cfd6bff
Ivan <34632454+abreulima@users.noreply.github.com>
2021-06-07 13:02:06 -0400

    First upload

---
+++ main.py
@@ -0,0 +1,8 @@
+# Image Detectition vs Image Classification
+
+#import tensorflow as tf
+import numpy as np
+import cv2 as cv
+#import matplotlib as plt
+
+#print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
